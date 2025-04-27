4ae6469a4ae0beb302762ecf53b028242f12ba3e
Tausif Rahman <69134814+tawsif0@users.noreply.github.com>
2025-04-27 12:39:42 +0600

    Update Background-circle.js

--- Background-circle.js
+++ Background-circle.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import ParticlesBg from 'particles-bg'; // <- Import Particles
-
+import DotsBackground from './Dots';
 
 const heroCircle = () => {
     return (
         <section className="hero position-relative">
             {/* Particle Background */}
             <ParticlesBg type="circle" bg={true} />
+         <DotsBackground />
         </section>
     );
 };
